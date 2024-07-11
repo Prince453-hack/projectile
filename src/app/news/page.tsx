19d0c6773c5ad369d5e391848227c3ad536ad7f9
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-07-11 19:26:14 +0530

    news section

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import React, { useState, useEffect } from "react";
 
 interface NewsArticle {
@@ -7,12 +10,14 @@ interface NewsArticle {
   link: string;
   pubDate: string;
   source_id: string;
+  image_url: string;
 }
 
 const NewsFeed = () => {
   const [articles, setArticles] = useState<NewsArticle[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [visibleCount, setVisibleCount] = useState(9);
 
   useEffect(() => {
     const apiKey = "pub_483776c294ef4dfc80dafa8f9732d05809c1a";
@@ -37,26 +42,73 @@ const NewsFeed = () => {
     fetchNews();
   }, []);
 
+  const handleShowMore = () => {
+    setVisibleCount(visibleCount + 5);
+  };
+
   return (
-    <div>
+    <div className="max-w-6xl mx-auto px-4 py-8 min-h-screen">
+      <Link href="/">
+        <p className="flex items-center justify-start">
+          <ArrowLeft className="mr-4" size={30} />
+          <img src="/fav/favicon.ico" className="h-10 w-10" />
+        </p>
+      </Link>
+      <h1 className="my-10 text-center text-5xl">Today&apos;s News</h1>
+
       {isLoading ? (
-        <p>Loading news...</p>
+        <p className="text-center text-2xl animate-pulse">Loading news...</p>
       ) : error ? (
         <p>Error: {error}</p>
       ) : (
-        articles.map((article, index) => (
-          <div key={index}>
-            <h3>{article.title}</h3>
-            <p>{article.description || "No description available."}</p>
-            <a href={article.link} target="_blank" rel="noopener noreferrer">
-              Read more
-            </a>
-            <p>
-              Published on: {new Date(article.pubDate).toLocaleDateString()}
-            </p>
-            <p>Source ID: {article.source_id}</p>
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+            {articles.slice(0, visibleCount).map((article, index) => (
+              <div
+                key={index}
+                className="bg-white rounded-lg shadow-lg p-2 mb-2"
+              >
+                <h3 className="text-lg font-semibold mb-1 text-black">
+                  {article.title}
+                </h3>
+                {article.image_url && (
+                  <img
+                    src={article.image_url || "/Article1.jpg"}
+                    alt={article.title}
+                    className="w-full h-[15rem] rounded-lg mb-1"
+                  />
+                )}
+                <p className="text-gray-700 mb-1 text-sm description">
+                  {article.description || "Read More on the link below."}
+                </p>
+                <a
+                  href={article.link}
+                  className="text-blue-500 hover:underline text-sm"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read more
+                </a>
+                <p className="text-gray-600 mt-1 text-xs">
+                  Published on: {new Date(article.pubDate).toLocaleDateString()}
+                </p>
+                <p className="text-gray-600 text-xs">
+                  Source ID: {article.source_id}
+                </p>
+              </div>
+            ))}
           </div>
-        ))
+          {/* {visibleCount < articles.length && (
+            <div className="flex justify-center mt-6">
+              <button
+                onClick={handleShowMore}
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              >
+                Show More
+              </button>
+            </div>
+          )} */}
+        </>
       )}
     </div>
   );
