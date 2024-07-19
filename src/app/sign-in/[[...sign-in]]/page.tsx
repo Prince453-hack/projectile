bc082628cc78409169295449ecff842e1ea87041
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-07-19 16:07:48 +0530

    sign in ui change

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,30 @@
-import { SignIn } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="w-full min-h-screen flex items-center justify-center">
-      <SignIn />
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or Create an account
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+
+      <div className="h-full bg-[#04005C] hidden lg:flex items-center justify-center">
+        <Image src="/main-logo.jpg" alt="Secureleap" width={500} height={500} />
+      </div>
     </div>
   );
 }
