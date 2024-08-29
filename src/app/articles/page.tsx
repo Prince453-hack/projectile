5d4ba5bea5572dea89a374908f85647a3fef8223
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-08-29 18:04:02 +0530

    card ui fixes

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -15,11 +15,11 @@ import {
 } from "@/components/ui/dialog";
 import Link from "next/link";
 
-const page = () => {
+const Page = () => {
   return (
-    <div className="min-h-screen dark:text-black">
+    <div className="min-h-screen dark:text-black dark:bg-white">
       <div className="bg-[#B8ECB6] h-[25vh] md:h-[30vh] lg:h-[50vh]">
-        <div className="flex items-center ml-10 md:ml-12 lg:ml-20 pt-10 md:pt-12 lg:pt-20 font-semibold">
+        <div className="flex items-center ml-4 md:ml-8 lg:ml-12 pt-4 md:pt-8 lg:pt-16 font-semibold">
           <Link href="/">
             <h1>Home</h1>
           </Link>
@@ -27,16 +27,16 @@ const page = () => {
           <p>TSL Articles</p>
         </div>
 
-        <div className="ml-10 md:ml-12 lg:ml-20 mt-10 md:mt-12 lg:mt-20">
-          <h1 className="font-extrabold text-3xl md:text-6xl uppercase">
+        <div className="ml-4 md:ml-8 lg:ml-12 mt-4 md:mt-8 lg:mt-16">
+          <h1 className="font-extrabold text-2xl md:text-4xl lg:text-6xl uppercase">
             TSL Articles
           </h1>
         </div>
 
-        <div className="">
-          <Card className="dark:bg-white dark:text-black relative h-[15vh] lg:h-[25vh] w-[30vh] lg:w-[50vh] mt-10 md:mt-16 lg:mt-10 ml-10 md:ml-12 lg:ml-20 rounded-xl">
+        <div className="ml-3 md:ml-8 lg:ml-12 mt-4 lg:mt-12">
+          <Card className="dark:bg-white dark:text-black relative h-[18vh] md:h-[23vh] lg:h-[25vh] w-[80vw] md:w-[45vw] lg:w-[30vw] mt-4 rounded-xl">
             <CardHeader className="p-3 md:p-5 lg:p-6">
-              <CardTitle className="text-base lg:text-2xl my-2 lg:my-5">
+              <CardTitle className="text-lg md:text-xl lg:text-2xl my-2 lg:my-5">
                 INSIGHTS POWER PLANT CHEMISTRY
               </CardTitle>
               <CardDescription className="font-semibold">
@@ -57,8 +57,9 @@ const page = () => {
                     <DialogDescription>
                       <iframe
                         src="https://newsletterspdf.s3.amazonaws.com/chemistry.pdf#toolbar=0"
-                        width="100%"
-                        height="600px"
+                        title="Insights Power Plant Chemistry"
+                        className="w-full"
+                        height="500"
                         style={{ border: "none" }}
                       ></iframe>
                     </DialogDescription>
@@ -73,4 +74,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
