6985911949949b9f498da30eee0d5d44bf60c388
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-11-23 19:41:51 +0530

    changes

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Home } from "lucide-react";
+import { AlertCircle, Home } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 const ErrorPage = () => {
   return (
-    <div className="min-h-screen bg-gray-200 flex items-center justify-center">
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="flex flex-col space-y-2 items-center text-center">
+        <AlertCircle className="size-8" />
         <p className="text-lg font-semibold">Application Error</p>
         <Button className="flex items-center justify-center" variant="outline">
           <Home className="size-4 mr-1" />
