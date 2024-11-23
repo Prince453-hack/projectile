395dec892e63eb0c3a94fb2a8e7141ca8af15281
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-11-23 16:14:42 +0530

    ui changes

--- src/components/TeamMembers.tsx
+++ src/components/TeamMembers.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { contributingMembers, coreMembers } from "../../constant/teamMembers";
+import { cn } from "@/lib/utils";
 
 const TeamMembers = () => {
   return (
@@ -16,14 +17,17 @@ const TeamMembers = () => {
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-5">
-          {coreMembers.map(({ detail, img, name, profession }) => (
+          {coreMembers.map(({ detail, img, name, profession }, index) => (
             <div
-              className="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-visible relative mt-16 border border-1 border-black hover:scale-105 transition-all"
+              className="max-w-sm mx-auto bg-gray-200/70 rounded-lg shadow-lg overflow-visible relative mt-16 hover:scale-105 transition-all"
               key={name}
             >
               <div className="absolute inset-x-0 top-[-48px] flex justify-center">
                 <img
-                  className="w-24 h-24 rounded-full border-2 border-gray-300 object-cover"
+                  className={cn(
+                    "w-24 h-24 rounded-full border-2 border-gray-300",
+                    index === 2 ? "object-fill" : "object-cover"
+                  )}
                   src={img}
                   alt="Profile"
                 />
@@ -50,7 +54,7 @@ const TeamMembers = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-5">
           {contributingMembers.map(({ detail, img, name, profession }) => (
             <div
-              className="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-visible relative mt-16 border border-1 border-black hover:scale-105 transition-all"
+              className="max-w-sm mx-auto bg-gray-200/70 rounded-lg shadow-lg overflow-visible relative mt-16 hover:scale-105 transition-all"
               key={name}
             >
               <div className="absolute inset-x-0 top-[-48px] flex justify-center">
@@ -61,7 +65,7 @@ const TeamMembers = () => {
                 />
               </div>
               <div className="pt-16 pb-6 px-6 mb-20">
-                <p className="text-gray-700 mb-4 text-center">{detail}</p>
+                <p className="text-black mb-4 text-center">{detail}</p>
               </div>
               <div className="absolute bottom-2 left-4">
                 <h3 className="text-lg font-bold text-gray-900 mt-36">
