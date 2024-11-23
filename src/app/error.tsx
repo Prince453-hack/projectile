995898fdbd1383da8f9bd1a42738a3ba5b0de81a
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-11-23 17:48:34 +0530

    fixes

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -7,8 +7,8 @@ import React from "react";
 
 const ErrorPage = () => {
   return (
-    <div className="min-h-screen bg-gray-200">
-      <div className="flex flex-col space-y-2 items-center justify-center">
+    <div className="min-h-screen bg-gray-200 flex items-center justify-center">
+      <div className="flex flex-col space-y-2 items-center text-center">
         <p className="text-lg font-semibold">Application Error</p>
         <Button className="flex items-center justify-center" variant="outline">
           <Home className="size-4 mr-1" />
