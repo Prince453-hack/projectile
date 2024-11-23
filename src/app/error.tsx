f7a10415c53eec1bed272cd33dc00832e4efc2f6
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-11-23 17:45:03 +0530

    error page

---
+++ src/app/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Home } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-200">
+      <div className="flex flex-col space-y-2 items-center justify-center">
+        <p className="text-lg font-semibold">Application Error</p>
+        <Button className="flex items-center justify-center" variant="outline">
+          <Home className="size-4 mr-1" />
+          <Link href="/">Homepage</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
